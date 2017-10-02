f0b67bc3fc8c4aca7932b03bbb6f869d8b2e1973
GerogeWilde <vchgc.love@163.com>
2017-10-02 17:52:28 +0800

    课程结束，后续更新

---
+++ Linux云计算实训课/IntrouceMyself.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+    > File Name: IntrouceMyself.cpp
+    > Author: Qiao xu
+    > Mail: GerogeWilde123@gmail.com
+    > Created Time: 2017年09月29日 星期五 16时57分02秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"******************* Start ********************"<<endl;
+	cout<<"\n";
+	cout<<"Hello World"<<endl;
+	cout<<"\n";
+	cout<<"姓名：乔旭"<<endl;
+	cout<<"班级：光146"<<endl;
+	cout<<"专业：物联网"<<endl;
+	cout<<"未来方向：学术研究"<<endl;
+	cout<<"\n";
+	cout<<"Linux云计算笔记网址：120.77.255.87";
+	cout<<"\n";
+	cout<<"\n";
+	cout<<"******************   End  ********************"<<endl;
+	return 0;
+}
